380d7867359a231eadc1c8198d67ff4009057384
Rishabh5911 <rishabhpokhriyal259@gmail.com>
2026-03-22 23:06:46 +0530

    code updated

--- src/components/core/Dashboard/EditCourse/index.jsx
+++ src/components/core/Dashboard/EditCourse/index.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom"
 
 
 import { setCourse, setEditCourse } from "../../../../slices/courseSlice"
+import getFullDetailsOfCourse from "../../../../services/operations/courseDetailsAPI"
 import RenderSteps from "../AddCourse/RenderSteps"
 
 export default function EditCourse() {
